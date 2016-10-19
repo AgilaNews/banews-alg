ca661ebb070e74e58ee2e40983a3ab04baa3ae9a
limeng <lemon.li.hust@gmail.com>
2016-10-19 21:26:31 +0800

    update parameter of lda

--- userTopicInterest/trainTopicModel.py
+++ userTopicInterest/trainTopicModel.py
@@ -8,9 +8,6 @@ from datetime import date, datetime, timedelta
 import MySQLdb
 import json
 import pickle
-from nltk import word_tokenize
-from nltk import pos_tag
-from nltk.stem.lancaster import LancasterStemmer
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 import spacy
@@ -23,7 +20,8 @@ if sys.getdefaultencoding() != default_encoding:
 KDIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = '/data/mysqlBackup/banews'
 PREPROCESS_DATA_DIR = '/data/userTopicDis'
-MODEL_DIR = os.path.join(KDIR, 'data')
+#MODEL_DIR = os.path.join(KDIR, 'data')
+MODEL_DIR = os.path.join(KDIR)
 ATTRIBUTE_DIM = 9
 (NEWS_ID, SRC_URL, CHANNEL_ID, TITLE, SRC_NAME, \
         PUBLISH_TIME, FETCH_TIME, CONTENT, TYPE) = \
@@ -41,7 +39,7 @@ def stripTag(htmlStr):
     reg = re.compile(r'<[^>]+>', re.S)
     return reg.sub('', htmlStr)
 
-def loadData(dateObj):
+def loadData(dateObj, start_date, end_date):
     newsLst = []
     replacePunc = string.maketrans(string.punctuation,
             ' ' * len(string.punctuation))
@@ -51,36 +49,28 @@ def loadData(dateObj):
             vals = line.strip().split('\t')
             if len(vals) < ATTRIBUTE_DIM:
                 continue
+            fetchTime = float(vals[FETCH_TIME])
+            fetchTime = datetime.fromtimestamp(fetchTime).date()
+            if (fetchTime < start_date) or (fetchTime >= end_date):
+                continue
             textStr = vals[TITLE] + ' ' + stripTag(vals[CONTENT])
             textStr = textStr.encode('utf-8').translate(replacePunc)
-            newsLst.append(textStr.decode('utf-8'))
+            newsLst.append((vals[NEWS_ID], textStr.decode('utf-8')))
     return newsLst
 
-def preProcessNLTK(newsLst, validTags=['NN','JJ','NNP']):
-    preNewsLst = []
-    stemmer = LancasterStemmer()
-    for curNews in newsLst:
-        tokenLst = word_tokenize(curNews)
-        stemmedLst = []
-        for curWord in tokenLst:
-            stemmedLst.append(stemmer.stem(curWord))
-        wordTagLst = pos_tag(stemmedLst)
-        filteredTagLst = [word for (word, tag) \
-                in wordTagLst if tag in validTags]
-        preNewsLst.append(' '.join(filteredTagLst))
-    return preNewsLst
-
-def preProcessSPACY(dateObj, withPreprocess):
+def preProcessSPACY(dateObj, start_date, end_date, withPreprocess):
     fileName = dateObj.strftime('%Y%m%d') + '_preprocess.dat'
-    preNewsLst = []
+    preNewsDocLst = []
+    preNewsIdLst = []
     print 'preprocessing trainning data...'
     if withPreprocess:
         print 'loading trainning data...'
-        newsLst = loadData(dateObj)
+        newsLst = loadData(dateObj, start_date, end_date)
         print '%s news loaded...' % len(newsLst)
         en_nlp = spacy.load('en')
         with open(os.path.join(PREPROCESS_DATA_DIR, fileName), 'w') as fp:
-            for idx, curDoc in enumerate(en_nlp.pipe(newsLst,
+            (idLst, docLst) = zip(*newsLst)
+            for idx, curDoc in enumerate(en_nlp.pipe(docLst,
                 batch_size=50, n_threads=4)):
                 stemmedDocLst = []
                 for token in curDoc:
@@ -88,55 +78,73 @@ def preProcessSPACY(dateObj, withPreprocess):
                         continue
                     stemmedDocLst.append(token.lemma_)
                 stemmedDocStr = ' '.join(stemmedDocLst)
-                print >>fp, stemmedDocStr
-                preNewsLst.append(stemmedDocStr)
+                newsId = idLst[idx]
+                print >>fp, '%s,%s' % (newsId, stemmedDocStr)
+                preNewsDocLst.append(stemmedDocStr)
+            preNewsIdLst = idLst
     else:
         with open(os.path.join(PREPROCESS_DATA_DIR, fileName), 'r') as fp:
             for line in fp:
-                preNewsLst.append(line)
-    return preNewsLst
+                vals = line.strip().split(',', 1)
+                if len(vals) != 2:
+                    continue
+                (newsId, newsDoc) = vals
+                preNewsDocLst.append(newsDoc)
+                preNewsIdLst.append(newsId)
+    return (preNewsIdLst, preNewsDocLst)
 
-def trainLDA(dateObj, withPreprocess=False):
-    preNewsLst = preProcessSPACY(dateObj, withPreprocess)
+def trainLDA(dateObj, start_date, end_date, withPreprocess=False):
+    (preNewsIdLst, preNewsDocLst) = preProcessSPACY(dateObj,
+            start_date, end_date, withPreprocess)
     print 'space vector model building...'
     vectorizer = CountVectorizer(max_df=MAX_DF,
                                  min_df=MIN_DF,
                                  max_features=MAX_FEATURES,
                                  stop_words='english')
-    tfMatrix = vectorizer.fit_transform(preNewsLst)
+    tfMatrix = vectorizer.fit_transform(preNewsDocLst)
     ldaModel = LatentDirichletAllocation(
                     n_topics=N_TOPIC,
                     max_iter=MAX_ITER,
-                    learning_method='online')
+                    learning_method='online',
+                    n_jobs=4,
+                    verbose=1,
+                    evaluate_every=5)
     print 'training lda model...'
-    ldaModel.fit(tfMatrix)
-    with open(os.path.join(MODEL_DIR, 'countVector.m'), 'wb') as fp:
-        pickle.dump(tfMatrix, fp)
-    with open(os.path.join(MODEL_DIR, 'lda.m'), 'wb') as fp:
-        pickle.dump(ldaModel, fp)
+    newsTopicArr = ldaModel.fit_transform(tfMatrix)
     print 'dumping model...'
+    dump(vectorizer, ldaModel, newsTopicArr, preNewsIdLst)
     return (vectorizer, ldaModel)
 
-def getDocTopics():
-    with open(os.path.join(MODEL_DIR, 'lda.m'), 'rb') as fp:
-        ldaModel = pickle.load(fp)
-    with open(os.path.join(MODEL_DIR, 'countVector.m'), 'rb') as fp:
-        tfMatrix = pickle.load(fp)
-        print tfMatrix
-        print type(tfMatrix)
-
-def debug(ldaModel, vectorizer, topWords=30):
-    featureNameLst = vectorizer.get_feature_names()
-    for topicIdx, topicDis in enumerate(ldaModel.components_):
-        print 'Topic:', topicIdx
-        topWordStr = " ".join([featureNameLst[idx] for idx \
-                in topicDis.argsort()[:-topWords-1:-1]])
-        print '\t', topWordStr
+def dump(vectorizer, ldaModel, newsTopicArr, preNewsIdLst,
+        topWords=100):
+    with open(os.path.join(MODEL_DIR, 'vectorizer.m'),
+            'wb') as fp:
+        pickle.dump(vectorizer, fp)
+    with open(os.path.join(MODEL_DIR, 'ldaModel.m'),
+            'wb') as fp:
+        pickle.dump(ldaModel, fp)
+    with open(os.path.join(MODEL_DIR, 'newsTopic.d'),
+            'w') as fp:
+        for idx, topicArr in enumerate(newsTopicArr):
+            newsId = preNewsIdLst[idx]
+            print >>fp, '%s\t%s' % (newsId,
+                    ','.join(map(str, topicArr)))
+    with open(os.path.join(MODEL_DIR, 'topicWord.d'),
+            'w') as fp:
+        featureNameLst = vectorizer.get_feature_names()
+        for topicIdx, topicDis in enumerate(ldaModel.components_):
+            print 'Topic:', topicIdx
+            topWordStr = " ".join([featureNameLst[idx] for idx \
+                    in topicDis.argsort()[:-topWords-1:-1]])
+            print '\t', topWordStr
+            print >>fp, '%s,%s' % (topicIdx, topWordStr)
 
 if __name__ == '__main__':
     if sys.argv[1] == 'train':
         dateObj = date(2016, 9, 18)
-        (vectorizer, ldaModel) = trainLDA(dateObj, withPreprocess=True)
-        debug(ldaModel, vectorizer)
+        end_date = date.today() + timedelta(days=1)
+        start_date = end_date - timedelta(days=180)
+        trainLDA(dateObj, start_date, end_date,
+                withPreprocess=True)
     elif sys.argv[1] == 'test':
-        getDocTopics()
+        pass
