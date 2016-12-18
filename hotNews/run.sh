4f64f30522a3ac6184920497c1e3ba0ae329a8d0
limeng <lemon.li.hust@gmail.com>
2016-12-18 14:08:58 +0800

    fix spark core number

--- hotNews/run.sh
+++ hotNews/run.sh
@@ -3,7 +3,7 @@ export PYSPARK_PYTHON=/usr/bin/python2.7
 
 /home/work/spark-1.6.2-bin-ba/bin/spark-submit \
     --master yarn-client --executor-memory 1G \
-    --num-executors 10 --executor-cores 4 \
+    --num-executors 3 --executor-cores 3 \
     --driver-memory 2G --conf spark.akka.frameSize=100 \
     --conf spark.shuffle.manager=SORT \
     --conf spark.yarn.executor.memoryOverhead=2048 \
